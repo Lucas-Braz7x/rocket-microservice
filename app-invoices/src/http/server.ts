740096165c44918aae281be9239bec403758d06a
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2025-06-07 17:42:21 -0300

    :tada: initial commit app-invoices

---
+++ app-invoices/src/http/server.ts
@@ -0,0 +1,22 @@
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifyCors, { origin: "*" });
+
+app.get("/health-check", () => {
+  return "App rodando normalmente!!!";
+});
+
+app.listen({ host: "0.0.0.0", port: 3333 }).then(() => {
+  console.log("[INVOICES] Tô rodandooooooooo");
+});
