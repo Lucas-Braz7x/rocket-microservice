6eeaa9a93f876fd679b56b4d116e725f50d8c6fc
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2025-06-07 13:46:02 -0300

    :wrench: chore: configurando db e rabbitmq

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # rocket-microservice
+
+# comandos do Drizzle-kit
+
+- `npx drizzle-kit generate`
+- `npx drizzle-kit migrate`
---
+++ app-orders/src/db/schema/orders.ts
@@ -0,0 +1,17 @@
+import { timestamp } from "drizzle-orm/pg-core";
+import { pgEnum } from "drizzle-orm/pg-core";
+import { pgTable, text, integer } from "drizzle-orm/pg-core";
+
+export const orderStatusEnum = pgEnum("order_status", [
+  "pending",
+  "paid",
+  "canceled",
+]);
+
+export const orders = pgTable("orders", {
+  id: text().primaryKey(),
+  customerId: text().notNull(),
+  amount: integer().notNull(),
+  status: orderStatusEnum().notNull().default("pending"),
+  createdAt: timestamp().defaultNow().notNull(),
+});
